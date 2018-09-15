1252d22c8dae6649cf873760c63f1ff7a0709c58
rio.karim <rio@toumetis.com>
2018-09-15 19:15:02 +0100

    init

--- bin/submit.php
+++ bin/submit.php
@@ -1,39 +1,27 @@
 <?php
-     if(filter_has_var(INPUT_POST, 'submit')){
-        $recaptcha_secret = "6Lefe2IUAAAAALTm9p2fRhkeWWKjzR7gFrND0Xgt";
-        $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$recaptcha_secret."&response=".$_POST['g-recaptcha-response']);
-        $response = json_decode($response, true);
-        $name = htmlspecialchars($_POST['name']);
-        $email = htmlspecialchars($_POST['email']);
-        if($response["success"] === true){
-             $toEmail = 'info@mrcryptoacademy.com';
-            $subject = 'New Lead - ' .$name;
-            $body = '
-             <h3> You have a Mr Crypto Lead -' .$name .'</h3>
-            <h4>Contact Details</h4>
-            <ul>
-                <li>First name:' .$name .'</li>
-                <li>Email:' .$email .'</li>
-            </ul>
-            ';
-            $headers = "MIME-Version: 1.0" ."\r\n";
-            $headers .="Content-Type:text/html;charset=UTF-8" ."\r\n";
+date_default_timezone_set("Europe/London");
 
-            $headers .= "From: Mr. Crypto Academy <noreply@mrcryptoacademy.co.uk>" ."\r\n";
-            if(mail($toEmail, $subject, $body, $headers)){
-                header("Location: ../product.html");
-                die();
-            }
-            else {
-                header("Location: ../fail.html");
-                die();
-            }
-        }
-        else {
-            header("Location: ../fail.html");
-            die();
-        }
+$current_date = date('d/m/Y | H:i:s');
+if(filter_has_var(INPUT_POST, 'submit')){
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    if(!empty($email) && !empty($name)){
+        $to = 'rkarim@websoar.co.uk';
+        $subject = 'New Lead - ' .$name;
+        $body = '
+         <h3> You have a Mr Crypto Lead - ' .$name .'</h3>
+        <h4>Contact Details</h4>
+        <ul>
+            <li>Date:' .$current_date .'</li>
+            <li>First name:' .$name .'</li>
+            <li>Email:' .$email .'</li>
+        </ul>
+        ';
+        $headers = "MIME-Version: 1.0" ."\r\n";
+        $headers .="Content-Type:text/html;charset=UTF-8" ."\r\n";
+        $headers .= "From: Mr Crypto Academy <noreply@mrcryptoacademy.com>" ."\r\n";
+        mail($to, $subject, $body, $headers);
     }
-
+}
 ?>
 
--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  $('.js-userbar .user-bar__content:last-child span').html('<span>' +callCustomer() +'</span>' + callMessage())
+  $('.js-userbar .user-bar__content:last-child span').html('<span>' +callCustomer() +'</span>' + callMessage());
   setInterval(function(){
     $('.js-userbar').toggleClass('visible');
     if (!$('.js-userbar').hasClass('visible')){
@@ -8,7 +8,11 @@ $(document).ready(function(){
         $('.js-userbar .user-bar__content:last-child span').html('<span>' +callCustomer() +'</span>' + callMessage())
       }, 2000)
     }
-  }, 6500)
+  }, 6500);
+	$('.btn-product:first-child').click( function() {
+		$('html, body').animate({
+			scrollTop: $(".section-buynow").offset().top},1000);
+	})
   $('.nav-list .nav-list__item, img[alt="Logo"], .nav-bar--mobile ul li').click( function(){
     switch(parseInt($(this).attr('data-nav'))){
       case 0:
@@ -32,20 +36,41 @@ $(document).ready(function(){
         }, 1500);
         break;
     }
-  })
+  });
   $('.btn-form').click(function(){
     $('html, body').animate({
       scrollTop: $("form").offset().top
     }, 2000);
-  })
-
+  });
   $('.btn-cookies').click(function(){
     $(this).parent().parent().remove();
-  })
+  });
 
   var navMobile= $('.nav-bar--close, #hamburger-icon');
   navMobile.click(function() {
     $('nav.nav-bar, nav.nav-bar--mobile, .nav-bar--mobile-filter').toggleClass('active');
     return false;
   });
+
+  fetch('bin/locale.json')
+      .then(function(response) {
+          return response.json();
+      })
+      .then(function(localeData) {
+          $.each(localeData, function(key, value){
+			  $("[locale-data]").each(function(){
+				  var div = $(this);
+				  if (div.attr('locale-data') == key){
+				    if (value.includes('http') || value.includes('https')){
+				      div.attr('href', value);
+                      console.log(div)
+                    }
+                    if (value.includes('mp4') || value.includes('ogg')){
+                      div.attr('src', value);
+                      console.log(div)
+                    }
+                  }
+			  })
+          })
+      });
 });
